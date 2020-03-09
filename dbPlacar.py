ef930c06c4349c49b87606a2092cdfec2c23e1e2
Bruno Fernandes <bwendel26999@gmail.com>
2020-03-09 04:59:56 -0300

    Criando arqv dbPlacar.py, para modificacoes futuras...

---
+++ dbPlacar.py
@@ -0,0 +1,6 @@
+"""
+Futura logica de conexao com banco de dados
+para adicionar um placar em  uma interface
+grafica com pyQT e mySQL, para mostrar os
+jogadores que mais ganharam e/ou mais recentes.
+"""
